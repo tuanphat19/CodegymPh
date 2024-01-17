0dfd3690a9a27f69762311aa96fe42dae120c872
tuanphat19 <buituanphat97@gmail.com>
2024-01-17 19:48:11 +0700

    dua file len github

---
+++ Vehinh.py
@@ -0,0 +1,14 @@
+# Bước 1: lấy công cụ vẽ
+import turtle
+# Bước 2: lấy ngòi viết
+t = turtle.Turtle()
+# Bước 3:
+#Tiến tới 100 đơn vị
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+t.right(90)
+t.forward(100)
+turtle.done()
